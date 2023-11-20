8eaa8afdf66b560302791e63ea8d36f330363418
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-11-20 14:46:05 +0100

    fix compatibility with NeoForge 20.2.59-beta

--- src/main/java/bl4ckscor3/mod/horizontalreinforceddeepslate/HorizontalReinforcedDeepslate.java
+++ src/main/java/bl4ckscor3/mod/horizontalreinforceddeepslate/HorizontalReinforcedDeepslate.java
@@ -1,20 +1,18 @@
 package bl4ckscor3.mod.horizontalreinforceddeepslate;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.ForgeRegistries;
-import net.neoforged.neoforge.registries.RegistryObject;
 
 @Mod(HorizontalReinforcedDeepslate.MODID)
 public class HorizontalReinforcedDeepslate {
 	public static final String MODID = "horizontalreinforceddeepslate";
-	private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-	public static final RegistryObject<HorizontalReinforcedDeepslateBlock> HORIZONTAL_REINFORCED_DEEPSLATE = BLOCKS.register("horizontal_reinforced_deepslate", () -> new HorizontalReinforcedDeepslateBlock(Properties.copy(Blocks.REINFORCED_DEEPSLATE).pushReaction(PushReaction.BLOCK)));
+	private static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+	public static final DeferredBlock<HorizontalReinforcedDeepslateBlock> HORIZONTAL_REINFORCED_DEEPSLATE = BLOCKS.register("horizontal_reinforced_deepslate", () -> new HorizontalReinforcedDeepslateBlock(Properties.copy(Blocks.REINFORCED_DEEPSLATE).pushReaction(PushReaction.BLOCK)));
 
 	public HorizontalReinforcedDeepslate() {
 		BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
