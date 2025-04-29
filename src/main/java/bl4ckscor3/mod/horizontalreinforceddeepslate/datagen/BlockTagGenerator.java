383b13027a8e93b202ec841ddbb86340ff166a54
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-29 10:48:06 +0200

    Update files to 1.21.5

--- src/main/java/bl4ckscor3/mod/horizontalreinforceddeepslate/datagen/BlockTagGenerator.java
+++ src/main/java/bl4ckscor3/mod/horizontalreinforceddeepslate/datagen/BlockTagGenerator.java
@@ -10,11 +10,10 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 public class BlockTagGenerator extends BlockTagsProvider {
-	public BlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, ExistingFileHelper existingFileHelper) {
-		super(output, lookupProvider, HorizontalReinforcedDeepslate.MODID, existingFileHelper);
+	public BlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+		super(output, lookupProvider, HorizontalReinforcedDeepslate.MODID);
 	}
 
 	@Override
