d30d47716a2c82b4e14557f9127d05f61f74cce3
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-12-11 00:00:41 +0100

    Update to 1.21.11

--- src/main/java/bl4ckscor3/mod/horizontalreinforceddeepslate/datagen/DataGenRegistrar.java
+++ src/main/java/bl4ckscor3/mod/horizontalreinforceddeepslate/datagen/DataGenRegistrar.java
@@ -1,31 +1,16 @@
 package bl4ckscor3.mod.horizontalreinforceddeepslate.datagen;
 
-import java.util.Optional;
-
 import bl4ckscor3.mod.horizontalreinforceddeepslate.HorizontalReinforcedDeepslate;
-import net.minecraft.DetectedVersion;
-import net.minecraft.data.metadata.PackMetadataGenerator;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
-import net.minecraft.util.InclusiveRange;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.EventBusSubscriber.Bus;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-@EventBusSubscriber(modid = HorizontalReinforcedDeepslate.MODID, bus = Bus.MOD)
+@EventBusSubscriber(modid = HorizontalReinforcedDeepslate.MODID)
 public class DataGenRegistrar {
 	private DataGenRegistrar() {}
 
 	@SubscribeEvent
 	public static void onGatherData(GatherDataEvent.Client event) {
 		event.createProvider(BlockTagGenerator::new);
-		//@formatter:off
-		event.createProvider(output -> new PackMetadataGenerator(output)
-                .add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("Horizontal Reinforced Deepslate resources & data"),
-                        DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
-                        Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE)))));
-		//@formatter:on
 	}
 }
